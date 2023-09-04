d69d48b8dd3825b760c0545b29dbf6bb21b685d8
welnage <31393351+welnage@users.noreply.github.com>
2023-09-04 20:15:20 +0000

    Adding python dictionaries

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+JOBS = [
+{
+  'id' : 1,
+  'title': 'Data Analysis',
+  'location': 'New Jersey',
+  'Salary': '100,000'
+},
+{
+  'id':2,
+  'title': 'Computer Science',
+  'location': 'New York',
+  'Salary': '120,000'
+},
+
+{
+  'id':3,
+  'title': 'Technical support',
+  'location': 'Georgia',
+  'Salary': '90,000'
+},
+
+{
+  'id':4,
+  'title': 'back-end Engineering',
+  'location': 'Remote',
+  'Salary': '130,000'
+}
+]
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS,company_name='welvens')
 
 if __name__ =='__main__':
   app.run(host='0.0.0.0', debug=True)
